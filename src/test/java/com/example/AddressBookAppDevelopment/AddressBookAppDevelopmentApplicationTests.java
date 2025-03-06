51896dab9020356d3c1bcabfb77760d212f38ffc
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-03-06 23:07:57 +0530

    Initializing MAIN

---
+++ src/test/java/com/example/AddressBookAppDevelopment/AddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookAppDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
