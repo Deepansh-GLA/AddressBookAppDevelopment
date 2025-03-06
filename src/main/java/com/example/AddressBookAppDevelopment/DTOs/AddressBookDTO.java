705319fc921a643e6e2289545f4ade93636f5e73
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-03-06 23:53:17 +0530

    Pushing Section2 UC2

--- src/main/java/com/example/AddressBookAppDevelopment/DTOs/AddressBookDTO.java
+++ src/main/java/com/example/AddressBookAppDevelopment/DTOs/AddressBookDTO.java
@@ -1,6 +1,5 @@
 package com.example.AddressBookAppDevelopment.DTOs;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
---
+++ src/main/java/com/example/AddressBookAppDevelopment/service/AddressBookService.java
@@ -0,0 +1,15 @@
+package com.example.AddressBookAppDevelopment.service;
+
+
+import com.example.AddressBookApp.DTOs.AddressBookDTO;
+import com.example.AddressBookApp.model.AddressBook;
+
+import java.util.List;
+
+public interface AddressBookService {
+    List<AddressBookDTO> getAllAddresses();
+    AddressBookDTO getAddressById(Long id);
+    AddressBookDTO createAddress(AddressBookDTO addressBookDTO);
+    AddressBookDTO updateAddress(Long id, AddressBookDTO addressBookDTO);
+    void deleteAddress(Long id);
+}
